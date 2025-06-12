7834c59c0557f6c920a2970a3f4d51621c7aa8b0
Remo Inverardi <remo@inverardi.ch>
2025-06-12 13:37:38 +0200

    Simplify the build.

--- frontend/scripts/dev.js
+++ frontend/scripts/dev.js
@@ -1,23 +1,15 @@
-import { build, preview } from 'vite';
+import { build, preview } from "vite";
 
 (async () => {
-  try {
-    await preview({
-      BASE_URL: '/',
-      MODE: 'production',
-      DEV: false,
-      PROD: true,
-    });
-    console.log('Preview server is running...');
-    console.log('Listening on http://localhost:4173\n');
+  await build({
+    build: {
+      minify: false,
+      sourcemap: true,
+      target: "esnext",
+      watch: {},
+    },
+    mode: "development",
+  });
 
-    const watcher = await build({
-      build: {
-        watch: {},
-      },
-    });
-    console.log('Build in watch mode is running...');
-  } catch (err) {
-    console.error('Error:', err);
-  }
+  await preview();
 })();
