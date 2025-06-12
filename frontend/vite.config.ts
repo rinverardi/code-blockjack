9c1580cb43b6850442f3d437b2a47f0b288816aa
Remo Inverardi <remo@inverardi.ch>
2025-06-12 13:37:16 +0200

    Clean up.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  assetsInclude: ['**/*.bin'],
   build: {
     chunkSizeWarningLimit: 2048,
   },
